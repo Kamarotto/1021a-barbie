f938731a7a057d539c9cc2c98be3b39b6b403460
Kamarotto <gustavo.freitas2@estudante.ifms.edu.br>
2023-08-30 07:51:25 -0400

    fhfh

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,11 +1,20 @@
+
 import './Filme.css'
-import Imagem from '../cachorrofiadaputa.webp'
+import imagem1 from "..//img/fbarbie1.webp"
+import imagem2 from "..//img/fbarbie2.webp"
+import imagem3 from "..//img/fbarbie3.webp"
+import imagem4 from "..//img/fbarbie4.webp"
 
 function Filme(){
     return(
         <div className="filme-content">
-
-            <img src="{Imagem}" alt="" />
+            <div className='imagens'>   
+            <img className='img-meio' src={imagem2} alt="" />
+            <img className='img-meio' src={imagem3} alt="" />
+            <img className='img-meio' src={imagem1} alt="" />
+            <img className='img-meio' src={imagem4} alt="" />
+            </div>
+           
         
         </div>
     )
--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,5 +1,11 @@
 import './Footer.css'
 import Link from './../link/Link'
+import imagem from "../img/barbielogorodape.png"
+import imagem1 from "../img/logo-youtube.png"
+import imagem2 from "../img/logo-facebook.avif"
+import imagem3 from "../img/logo-instagram.png"
+import imagem4 from "../img/logo-twitter.avif"
+import imagem5 from "../img/logo-discord.png"
 
 
 export function Footer(){
@@ -7,12 +13,12 @@ export function Footer(){
       <footer>
         <nav>
             <div className='listas'>
-              <div className='logo-rodape'><img className='img-rodape' src="../src/componentes/barbielogorodape.png" alt="" />
-              <img className='imgs-rodape' src="../src/componentes/logo-instagram.png" alt="" />
-              <img className='imgs-rodape'  src="../src/componentes/logo-facebook.avif" alt="" />
-              <img className='imgs-rodape'  src="../src/componentes/logo-twitter.avif" alt="" />
-              <img className='imgs-rodape'  src="../src/componentes/logo-youtube.png" alt="" />
-              <img className='imgs-rodape'  src="../src/componentes/logo-discord.png" alt="" />
+              <div className='logo-rodape'><img className='img-rodape' src={imagem} alt="" />
+              <img className='imgs-rodape' src={imagem1} alt="" />
+              <img className='imgs-rodape' src={imagem2} alt="" />
+              <img className='imgs-rodape' src={imagem3} alt="" />
+              <img className='imgs-rodape' src={imagem4} alt="" />
+              <img className='imgs-rodape' src={imagem5} alt="" />
               </div>
                 <ul className='coisas-da-lista'>
                   <h3 className='titulo-rodape'>Produtos</h3>
