cf53a1170b39f77364f3046b02e97b26cd33b152
Kamarotto <gustavo.freitas2@estudante.ifms.edu.br>
2023-08-30 07:51:12 -0400

    tarefabarbie

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Header from './componentes/header/Header'
 import Footer from './componentes/footer/Footer'
+import Filme from './componentes/filme/Filme'
+
+
 function App() {
   return(
     <div>
     <Header/>
+    <Filme/>
     <Footer/>
     </div>
   )
---
+++ src/componentes/filme/Filme.tsx
@@ -0,0 +1,14 @@
+import './Filme.css'
+import Imagem from '../cachorrofiadaputa.webp'
+
+function Filme(){
+    return(
+        <div className="filme-content">
+
+            <img src="{Imagem}" alt="" />
+        
+        </div>
+    )
+}
+
+export default Filme
